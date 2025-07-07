f6cc607657ad65a682f7258537d119a827c59e1b
torbenkeller <torben.keller.develop@gmail.com>
2025-07-07 11:40:54 +0200

    refactored calender

--- src/frontend/lib/behandlungen/presentation/behandlungen_calendar_provider.dart
+++ src/frontend/lib/behandlungen/presentation/behandlungen_calendar_provider.dart
@@ -3,13 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:physio_ai/behandlungen/domain/behandlung.dart';
 import 'package:physio_ai/behandlungen/infrastructure/behandlungen_repository.dart';
 
-final weeklyCalendarProvider =
-    FutureProvider.family<IList<BehandlungKalender>, DateTime>((ref, date) async {
-  final repository = ref.watch(behandlungenRepositoryProvider);
-  final dateString = date.toIso8601String().split('T')[0]; // Format: YYYY-MM-DD
-  final behandlungenMap = await repository.getWeeklyCalendar(dateString);
-  return behandlungenMap.values.fold<IList<BehandlungKalender>>(
-    IList(),
-    (f, elem) => f.addAll(elem),
-  );
-});
+final weeklyCalendarProvider = FutureProvider.family<IList<BehandlungKalender>, DateTime>(
+  (ref, date) async {
+    final repository = ref.watch(behandlungenRepositoryProvider);
+    final dateString = date.toIso8601String().split('T')[0]; // Format: YYYY-MM-DD
+    final behandlungenMap = await repository.getWeeklyCalendar(dateString);
+    return behandlungenMap.values.fold<IList<BehandlungKalender>>(
+      IList(),
+      (f, elem) => f.addAll(elem),
+    );
+  },
+);
